05845e8cec2739574d5697b888803b7182fda024
AbeJLazaro <abrahamlazaro@comunidad.unam.mx>
2020-12-06 13:23:44 -0600

    .gitignore, modifiqué el modelado de los elementos punteados, comenté todo lo posible

--- autoLR1.py
+++ autoLR1.py
@@ -1,80 +1,123 @@
+'''
+Autor:        Lázaro Martínez Abraham Josué
+Titulo:       autoLR1.py
+Versión:      1.0
+Fecha:        6 de diciembre de 2020
+'''
 from leergramatica import informacion
 
 # ***************************************************************
 
 # cerradura *****************************************************
 def combinar(beta,a):
-  aux=[]
+  '''Función que permite generar las combinaciones de beta con los
+  posibles diferentes a
+
+  Parámetros
+  beta: porción de la producción que petenece a beta
+  a:    Simbolo de preorden, es una lista de ellos
+
+  return lista con las concatenaciones de beta y las posibles a
+  '''
+
+  # si beta no existe, es la cadena vacía, regresamos a
   if len(beta)==0:
-    return a
+    return [a]
+  # si no, 
   else:
-    for j in a:
-      betap=""
-      for i in beta:
-        betap+=i
-      aux.append(betap+j)
-    return aux
+    return beta+[a]
+
 def first(x):
-  aux = []
-  for i in x:
-    aux.extend(First[i[0]])
-  return aux
+  '''Función para calcular el first de una cadena
+
+  Parámetros
+  x: lista que representa la concatenación de beta y a
+
+  return lista con el conjunto first de las cadenas
+  '''
+  return First[x[0]]
 
 def cerradura(I):
+  '''
+  Función cerradura
+
+  Parámetros
+  I: Estado al cual se le aplica cerradura
+
+  return estado, conjunto de elementos punteados
+  '''
+
+  # generamos un conjunto J y un conjunto K que nos servirá como bandera
   J = I.copy()
   K = []
 
+  # para cada elemento punteado en J, agregamos una concatenación de listas
+  # representativas para cada elemento punteado
   for ep in J:
-    K.append([ep[0]]+ep[1]+ep[2]+[ep[3]])
+    K.append([ep[0]]+ep[1]+[ep[2]]+[ep[3]])
 
+  # para cada elemento punteado que se encuentra en J
   for ep in J:
-    #print("ep:",ep)
+    # si la producción es diferente a cadena vacía y el punto no se encuentra al
+    # final de la producción
     if ep[1]!=[""] and len(ep[1])>ep[-1]:
+      # calculamos B, beta y a
       B = ep[1][ep[-1]]
       beta =  ep[1][ep[-1]+1:]
       a = ep[2]
-      #print("B:",B)
-      #print("beta:",beta)
-      #print("a:",a)
+      # si B pertenece a los simbolos no terminales
       if B in N:
+        # para cada producción de B
         for prod in gramatica[B]:
-          #print(prod)
+          # calculamos la concatenación de beta y a
           comb = combinar(beta,a)
-          #print("comb:",comb)
+          # calculamos el first de esa combinación
           primeros = first(comb)
-          #print("primeros:",primeros)
+          # para cada elemento terminal en el conjunto first de la concatenación
           for terminal in primeros:
-            #print("terminal:",terminal)
-            #print([B]+prod+[terminal[0]]+[0])
-            if [B]+prod+[terminal[0]]+[0] not in K:
-              #print("Se agrega",[B,prod,First[terminal[0]],0])
-              J.append([B,prod,First[terminal[0]],0])
-              K.append([B]+prod+[terminal[0]]+[0])
-
-
-  #imprimirRenglones(J)
+            if [B]+prod+[terminal]+[0] not in K:
+              J.append([B,prod,terminal,0])
+              K.append([B]+prod+[terminal]+[0])
   return J
 
 # goto **********************************************************
 
 def goto(I,X):
+  '''Implementación de la función goto
+  Parámetros
+  I: estado 
+  X: simbolo gramatical
+
+  return estado nuevo
+  '''
+
+  # conjunto de elementos punteados inicializado
   J = []
+  # para cada elemento punteado en el estado I
   for ep in I:
-    if len(ep[1])>ep[-1]:
-      #print(ep[1][ep[-1]]==X)
-      if ep[1][ep[-1]]==X:
-        epp = ep.copy()
-        epp[-1]+=1
-        J.append(epp)
+    # si el punto no se encuentra al final de la producción
+    # y el simbolo de la producción donde se encuentra el punto es igual a X
+    if len(ep[1])>ep[-1] and ep[1][ep[-1]]==X:
+      # hacemos una copia del elemento punteado, y aumentamos la posición del 
+      # punto
+      epp = ep.copy()
+      epp[-1]+=1
+      # lo agregamos a J
+      J.append(epp)
+  # regresamos la cerradura del conjunto J
   return cerradura(J)
 
 # funciones para imprimir ***************************************
 def imprimirRenglones(x):
+  '''Función para debuggear'''
   for i in x:
     print(i)
 
 def imprimir(x):
-  # función para imprimir los estados mejorados
+  '''función para imprimir los estados en menos líneas
+  Parámetros
+  x: conjunto mejorado de elementos punteados (lista)'''
+
   # llave que abre
   print("{")
   # para cada elemento punteado dentro de los estados
@@ -106,26 +149,53 @@ def imprimir(x):
   print("}",end="")
 
 def imprimirEstado(X):
+  '''Función auxiliar para impresión de estados
+
+  Parámetros
+  X: Estado, conjunto de elementos punteados (lista)
+  '''
+  # creamos una lista nueva
   lista = []
+
+  # para i de 0 hasta la cantidad de elementos punteados en el estado -1
   for i in range(len(X)):
+    # creamos una copia del elemento punteado i
     aux=X[i].copy()
-    ele=[aux[0],aux[1].copy(),aux[2].copy(),aux[3]]
+    # creamos una copia de caaa lista que exista en el elemento punteado
+    ele=[aux[0],aux[1].copy(),[aux[2]],aux[3]]
+
+    # si la longitud de la lista es 0, agregamos el elemento punteado
     if len(lista)==0:
-      lista.append(ele.copy())
+      lista.append(ele)
+    # si no
     else:
+      # iniciamos una bandera como False, esta bandera nos indicará si el elemento
+      # punteado ya estaba dentro de la lista pero con a diferente
       bandera = False
+      # para cada elemento punteado dentro de la lista
       for j in range(len(lista)):
+        # creamos una copia del elemento de la lista
         elep = lista[j].copy()
+        # si la cabecera, producción y posición del punto son iguales
         if ele[0]==elep[0] and ele[1]==elep[1] and ele[3]==elep[3]:
+          # para cada elemento de a en el elemento a ingresar
           for o in ele[2]:
+            # si el simbolo a no se encuentra dentro del elemento punteado ya
+            # ingresado en la lista
             if o not in elep[2]:
+              # agregamos dicho elemento punteado
               elep[2].append(o)
+          # actualizamos el elemento punteado en la lista
           lista[j]=elep
+          # cambia la badera a True
           bandera = True
+      # si el elemento punteado no se encontraba dentro de la lista, se agrega
       if not bandera:
         lista.append(ele)
+  # se pide imprimir el estado
   imprimir(lista)
 
+# variables globales
 N = None
 M = None
 gramatica = None
@@ -133,8 +203,16 @@ inicial = None
 First = None
 
 def CalcularAutomata(nombre):
+  '''Función principal para calcular el autómata push down
+  Parámetros
+  nombre: nombre del archivo donde se encuentran las especificaciones de la
+          gramática
+
+  return tabla de transiciones (lista) y lista de estados'''
+
+  # se especifica que se utilizaran las variables globales
   global N,M,gramatica,inicial,First
-  # pruebas *******************************************************
+  # Se importan los datos con la función informacion() de leergramatica.py
   datos = informacion(nombre)
 
   # definición de la gramática **********************************
@@ -149,40 +227,58 @@ def CalcularAutomata(nombre):
   # [[cabecera,[producción],[prelectura],[indice del punto],...]
   # I0
   I0 =  [
-          [inicial,gramatica[inicial][0],["$"],0]
+          [inicial,gramatica[inicial][0],"$",0]
         ]
 
+  # inicialización de los estados y primer cerradura de la producción inicial
   estados = []
   estados.append(cerradura(I0))
+  # contadores en 0
   i = 0
   j = 0
+  # impresión del primer estado
   print("Cerradura({"+inicial+"→"+gramatica[inicial][0][0]+",$})=")
   imprimirEstado(estados[0].copy())
   print("}=I0")
   print("")
+  # generación de la tabla de transiciones
   tablita = []
+  # mientras j sea menor a la cantidad de estados generados
   while j<len(estados):
+    # tomamos el estado j
     I = estados[j]
+    # para cada simbolo gramatical
     for x in N+M:
+      # calculamos el goto de ese estado con cada simbolo
       aux = goto(I,x)
-      #print("goto(I"+str(j)+","+x+")=",aux)
+      # si nos arroja un estado 
       if aux != None and aux != []:
+        # imprimimos la transición
         print("goto(I"+str(estados.index(I))+","+x+")=",end="")
         imprimirEstado(aux.copy())
+        # si el estado obtenido ya se encuentra en la lista de estados
         if aux in estados:
+          # hacemos referencia a que estado es
           print("=I"+str(estados.index(aux)))
+          # agregamos la transición a la tabla de transiciones
           tablita.append(["I"+str(estados.index(I)),x,"I"+str(estados.index(aux))])
+        # si no
         else:
+          # agregamos el estado a la lista de estados, aumentamos el contador de i 
+          # e indicamos este nuevo estado
           estados.append(aux)
           i+=1
           print("=I"+str(i))
+          # agregamos la transición a la tabla de transiciones
           tablita.append(["I"+str(estados.index(I)),x,"I"+str(i)])
         print("")
-
+    # aumentamos el contador de j para visitar el siguiente estado
     j+=1
+  # terminando el ciclo, regresamos la tabla de transiciones y la lista de estados
   return tablita,estados
+  
 if __name__ == '__main__':
-  tablita,estados=CalcularAutomata("ejemplo2.txt")
+  tablita,estados=CalcularAutomata("b.txt")
   for linea in tablita:
     print(linea)
-  print(estados[1])
+  #print(estados[1])
--- tabla.py
+++ tabla.py
@@ -2,57 +2,90 @@ from autoLR1 import CalcularAutomata
 from leergramatica import informacion
 
 def error(x,y,ant,nue):
+  '''Función para indicar donde se encuentra un error'''
   print("Error en",x,y)
   print("valor anterior:",ant)
   print("siguiente valor:",nue)
 
 def tablaLR1(nombre,archivo=False):
+  '''implementación para la generación de la tabla LR1
+  Parámetros
+  nombre: nombre del archivo donde se encuentran las especificaciones de la gramática
+  archivo: default False, indica si se creará un archivo tipo csv con la tabla generada'''
+
+  # obtenemos los datos de la gramática con la función informacion()
   datos = informacion(nombre)
-  tablita,estados=CalcularAutomata(nombre)
+  # obtenemos la tabla de transiciones y los estados con CalcularAutomata()
+  transiciones,estados=CalcularAutomata(nombre)
 
+  # imprimimos cuantos estados existen
   print("estados:",len(estados))
-
-  for linea in tablita:
+  # imprimimos sus transiciones
+  for linea in transiciones:
     print(linea)
 
+  # inicializamos los datos en variables más comodas
   M=datos["M"]
   N=datos["N"]
   Producciones=datos["Producciones"]
   inicial=datos["Inicial"]
 
+  # creamos las colmnas y las filas como diccionarios
   columnas = dict([(x,"error") for x in M+N])
   tabla = dict([(x,columnas.copy()) for x in range(len(estados))])
+
+  # para i de 0 hasta el último estado
   for i in range(len(estados)):
+    # obtenemos el estado i
     estado = estados[i]
-    for renglon in tablita:
+    # para cada transición
+    for renglon in transiciones:
+      # si es una transición que viene del estado i
       if renglon[0]=="I"+str(i):
-        # print(renglon)
+        # si el simbolo gramatical que genera la transición es terminal
         if renglon[1] in M:
+          # si el espacio no esta vacío, se arroja un error
           if tabla[i][renglon[1]]!="error":
             error(i,renglon[1],tabla[i][renglon[1]],"d"+renglon[2][1:])
+          # si no, se agrega el desplazamiento en la casilla correpondiente
           else:
             tabla[i][renglon[1]]="d"+renglon[2][1:]
+        # si el simbolo gramatical que genera la transición es no terminal
         elif renglon[1] in N:
+          # si el espacio no esta vacío, se arroja un error
           if tabla[i][renglon[1]] != "error":
             error(i,renglon[1],tabla[i][renglon[1]],renglon[2][1:])
+          # si no, se agrega el desplazamiento en la casilla correpondiente
           else:
             tabla[i][renglon[1]]=renglon[2][1:]
+    # para cada elemento punteado en el estado
     for j in estado:
+      # filtramos las producciones vacias para dejarlas como una lista
+      # vacía, ya que se modelan como [""]
       jp = list(filter(lambda x: len(x)>0,j[1]))
+      # si el punto se encuentra al final de la producción
       if len(jp)==j[3]:
-        if j[2][0]=="$" and j[0]==inicial:
+        # si el simbolo de transición es $ y la cabecera es el simbolo inicial
+        if j[2]=="$" and j[0]==inicial:
+          # agregamos el valor de Aceptar en la casilla correspondiente siempre
+          # y cuando, no se encuentre ya agregado
           if tabla[i]["$"]!="error":
             error(i,"$",tabla[i]["$"],"Acep")
           else:
             tabla[i]["$"]="Acep"
+        # si no
         else:
+          # obtenemos una lista con la cabecera y la producción
           aux = [j[0],j[1]]
+          # obtenemos el indice de dicha producción
           indice = Producciones.index(aux)
-          if tabla[i][j[2][0]] != "error":
-            error(i,j[2][0],tabla[i][j[2][0]],"r"+str(indice))
+          # si está vacía la casilla, se agrega el operador reducir
+          if tabla[i][j[2]] != "error":
+            error(i,j[2],tabla[i][j[2]],"r"+str(indice))
           else:
-            tabla[i][j[2][0]]="r"+str(indice)
+            tabla[i][j[2]]="r"+str(indice)
 
+  # impresión de la tabla
   cabecera = ""
   for i in ["edo"]+M+N:
     cabecera+="|{0:^8}".format(i)
@@ -67,6 +100,8 @@ def tablaLR1(nombre,archivo=False):
     imprimible +="|\n"
 
   print(imprimible)
+
+  # generación del archivo de la tabla
   if archivo:
     cabecera = ""
     for i in ["edo"]+M+N:
@@ -87,4 +122,4 @@ def tablaLR1(nombre,archivo=False):
     archivo.close()
 
 if __name__ == '__main__':
-  tablaLR1("d.txt",archivo=True)
+  tablaLR1("f.txt",archivo=True)
